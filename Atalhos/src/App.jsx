df7a01d62af1e131b67dc9063ac0bb7cd9c9b273
Buschjulia <julia_busch@estudante.sesisenai.org.br>
2024-08-29 17:22:35 -0300

    Add criação de componentes e revisamos alguns atalhos

---
+++ Atalhos/src/App.jsx
@@ -0,0 +1,34 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Atalhos do VS Code</h1>
+      <h2>Atalhos para facilitar a vida de um dev</h2>
+      <ul>
+        <li>Comando para emmet da ul: ul +  + li + * + Número de repetições</li>
+        <li>Comando para escrever simultaneamete: apertar scroll do mouse, selecionar linhas e escrever</li>
+        <li>Atalho: alt + z para quebra automatica na linha</li>
+        <li>Atalho: alt + clique esquerdo para adicionar novos cursores na tela</li>
+        <li>Atalho: ctrl + shift + k para exluir uma linha inteira</li>
+        <li>Atalho: ctrl + ; para comentar/descomentar</li>
+        <li>Atalho: Windows + . abre o teclado de emojis</li>
+        <li>Atalho: alt + ↕ para mover uma linhha/bloco</li>
+        <li>Atalho: alt + shift + ↕ ccopia a linha atual/bloco selecionado </li>
+        <li>Atalho: ctrl + j ou ctrl + ' abre/fecha o terminal</li>
+      </ul>
+
+      <h2>Extensões</h2>
+      <ul>
+        <li>ES7 React/Redux/GraphQL/React-Native snippets</li>
+        <li>Auto rename tag</li>
+        <li>Color highlight</li>
+        <li>Power mode</li>
+        <li>VSCode Pets</li>
+      </ul>
+    </>
+  )
+}
+
+export default App
---
+++ React/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <h1>Dinossauros</h1>
+      <h2>Carnivoros</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt voluptate mollitia aperiam molestias odio hic voluptatibus eaque pariatur. Cupiditate velit cum ratione incidunt voluptates quis blanditiis similique amet vel magni.</p>
+    </>
+  )
+}
+
+export default App
